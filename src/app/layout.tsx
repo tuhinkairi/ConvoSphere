88d0197c6e652057bae1f6f066bf7acdd2487f46
tuhinkairi <tuhinkairii@gmail.com>
2024-10-26 00:23:09 +0530

    basic setup compleate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-	{children}
-</ThemeProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
