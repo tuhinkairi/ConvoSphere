55b175fe363806836444464c5eb01ded3cb53866
tuhinkairi <tuhinkairii@gmail.com>
2024-11-06 20:03:09 +0530

    Emoji added

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
 import type * as clerk from "../clerk.js";
 import type * as conversations from "../conversations.js";
 import type * as http from "../http.js";
+import type * as messages from "../messages.js";
 import type * as users from "../users.js";
 
 /**
@@ -30,6 +31,7 @@ declare const fullApi: ApiFromModules<{
   clerk: typeof clerk;
   conversations: typeof conversations;
   http: typeof http;
+  messages: typeof messages;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
--- src/components/home/message-input.tsx
+++ src/components/home/message-input.tsx
@@ -2,18 +2,58 @@ import { Laugh, Mic, Plus, Send } from "lucide-react";
 import { Input } from "../ui/input";
 import { useState } from "react";
 import { Button } from "../ui/button";
+import { useConversationStore } from "@/app/_store/chatStore";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import toast from "react-hot-toast";
+import useComponentVisible from "@/hooks/useComponentVisible";
+import EmojiPicker, { Theme } from 'emoji-picker-react';
 
 const MessageInput = () => {
+	// message sending
 	const [msgText, setMsgText] = useState("");
+	const { selectedConversation } = useConversationStore()
+
+	// custom hook
+	const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
+	
+	// query
+	const sendTextMsg = useMutation(api.messages.sendTextMessages)
+	const user = useQuery(api.users.getMe)
+
+	// handelfunctions
+	const handelSendMessage = async (e: React.FormEvent) => {
+		e.preventDefault()
+		try {
+			// sending the text data
+			await sendTextMsg({
+				sender: user!._id,
+				content: msgText,
+				conversation: selectedConversation!._id,
+			})
+			setMsgText('');
+		}
+		catch (err: any) {
+			toast.error(err.message);
+			console.log(err)
+		}
+	}
 
 	return (
 		<div className='bg-gray-primary p-2 flex gap-4 items-center'>
 			<div className='relative flex gap-2 ml-2'>
-				{/* EMOJI PICKER WILL GO HERE */}
-				<Laugh className='text-gray-600 dark:text-gray-400' />
+				{/* emoji section */}
+				<div ref={ref} onClick={() => setIsComponentVisible(true)} >
+					{isComponentVisible && <EmojiPicker theme={Theme.AUTO}
+						onEmojiClick={(e: any) => setMsgText(msgText + e.emoji)}
+						style={{ position: "absolute", bottom: "1.5rem", left: "1rem", zIndex: 50 }}
+					/>}
+					<Laugh className='text-gray-600 dark:text-gray-400' />
+				</div>
+				{/* media section */}
 				<Plus className='text-gray-600 dark:text-gray-400' />
 			</div>
-			<form className='w-full flex gap-3'>
+			<form onSubmit={handelSendMessage} className='w-full flex gap-3'>
 				<div className='flex-1'>
 					<Input
 						type='text'
