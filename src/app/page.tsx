33b6009cd27d0992b7382648d205feff7794624f
tuhinkairi <tuhinkairii@gmail.com>
2024-11-16 15:57:28 +0530

    login logic update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,20 @@
 
+"use client"
 import LandingPage from "@/components/home/landingpage";
 import LeftPanel from "@/components/home/LeftPanel";
 import RightPanel from "@/components/home/RightPanel";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { useClerk } from "@clerk/nextjs";
 export default function Home() {
+	const { user } = useClerk()
 	return (<main className='h-screen w-screen'>
-
-		<SignedOut>
-			<LandingPage/>
-		</SignedOut>
-
-
-		<SignedIn>
+		{!user ? <LandingPage /> :
 			<div className='flex overflow-y-hidden h-full max-w-[1700px] mx-auto bg-left-panel '>
 				{/* Green background decorator for Light Mode */}
 				<div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
 				<LeftPanel />
 				<RightPanel />
 			</div>
-		</SignedIn>
-
+		}
 	</main>
 	);
 }
